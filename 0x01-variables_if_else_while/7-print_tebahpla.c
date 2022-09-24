bbb9914f6f193a5d12ac0da0dbf3e756e625d962
edukeb <kebedeeden@gmail.com>
2022-09-23 20:03:41 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphab;
+
+	for (alphab = 'z'; alphab >= 'a'; alphab--)
+		putchar(alphab);
+
+	putchar('\n');
+
+	return (0);
+}
